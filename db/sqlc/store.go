51b96197a60705bc959347544d7b88b4f9ac4bcf
wins <win4709hi@gmail.com>
2023-05-16 19:38:36 +0800

    feat: query and store

---
+++ db/sqlc/store.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type Store interface {
+	Querier
+}
+
+type SQLStore struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
+		Queries: New(db),
+		db:      db,
+	}
+}
+
+func (store *SQLStore) ExecTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err : %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
