a09ac2bf578af273d281ba7a97c7486171f218f7
wins <win4709hi@gmail.com>
2023-05-17 11:27:58 +0800

    feat: update not null on migration

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Role (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30),
+    name VARCHAR(30) NOT NULL,
     description TEXT
 );
 
@@ -9,8 +9,8 @@ CREATE TABLE "user" (
     line_id VARCHAR(50) UNIQUE NOT NULL,
     name VARCHAR(150) NOT NULL,
     picture TEXT NOT NULL,
-    google_maps_api_call_count SMALLINT DEFAULT 5,
-    role_id INTEGER DEFAULT 2,
+    google_maps_api_call_count SMALLINT DEFAULT 5 NOT NULL,
+    role_id INTEGER DEFAULT 2 NOT NULL,
     FOREIGN KEY (role_id) REFERENCES Role(id)
 );
 
@@ -18,18 +18,18 @@ CREATE INDEX idx_user_line_id ON "user"(line_id);
 
 CREATE TABLE Restaurant (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(150),
+    name VARCHAR(150) NOT NULL,
     rating DECIMAL(3,2),
     user_ratings_total INTEGER,
-    address VARCHAR(255),
-    google_map_place_id VARCHAR(255),
-    google_map_url TEXT,
-    phone_number VARCHAR(25)
+    address VARCHAR(255) NOT NULL,
+    google_map_place_id VARCHAR(255) NOT NULL,
+    google_map_url TEXT NOT NULL,
+    phone_number VARCHAR(25) NOT NULL
 );
 
 CREATE TABLE User_Restaurant (
-    user_id INTEGER,
-    restaurant_id INTEGER,
+    user_id INTEGER NOT NULL,
+    restaurant_id INTEGER NOT NULL,
     PRIMARY KEY(user_id, restaurant_id),
     FOREIGN KEY (user_id) REFERENCES "user"(id),
     FOREIGN KEY (restaurant_id) REFERENCES Restaurant(id)
@@ -40,12 +40,12 @@ CREATE INDEX idx_user_restaurant_restaurant_id ON User_Restaurant(restaurant_id)
 
 CREATE TABLE Food (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(75),
-    price DECIMAL(5,2),
+    name VARCHAR(75) NOT NULL,
+    price DECIMAL(5,2) NOT NULL,
     image TEXT,
     description TEXT,
-    restaurant_id INTEGER,
-    version SMALLINT DEFAULT 0,
+    restaurant_id INTEGER NOT NULL,
+    version SMALLINT DEFAULT 0 NOT NULL,
     edit_by INTEGER,
     FOREIGN KEY (restaurant_id) REFERENCES Restaurant(id),
     FOREIGN KEY (edit_by) REFERENCES "user"(id)
@@ -55,8 +55,8 @@ CREATE INDEX idx_food_restaurant_id ON Food(restaurant_id);
 CREATE INDEX idx_food_edit_by ON Food(edit_by);
 
 CREATE TABLE User_Food (
-    user_id INTEGER,
-    food_id INTEGER,
+    user_id INTEGER NOT NULL,
+    food_id INTEGER NOT NULL,
     PRIMARY KEY(user_id, food_id),
     FOREIGN KEY (user_id) REFERENCES "user"(id),
     FOREIGN KEY (food_id) REFERENCES Food(id)
@@ -67,12 +67,12 @@ CREATE INDEX idx_user_food_food_id ON User_Food(food_id);
 
 CREATE TABLE Operate_Record (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER,
-    food_id INTEGER,
+    user_id INTEGER NOT NULL,
+    food_id INTEGER NOT NULL,
     before TEXT,
     after TEXT,
-    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    operate_category SMALLINT,
+    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    operate_category SMALLINT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES "user"(id),
     FOREIGN KEY (food_id) REFERENCES Food(id)
 );
@@ -82,11 +82,11 @@ CREATE INDEX idx_operate_record_update_at ON Operate_Record(update_at);
 
 CREATE TABLE Feedback (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER,
-    food_id INTEGER,
-    edit_by INTEGER,
-    created_at TIMESTAMP DEFAULT NOW(),
-    "status" VARCHAR(20) DEFAULT 'todo' CHECK ("status" IN ('todo', 'done')),
+    user_id INTEGER NOT NULL,
+    food_id INTEGER NOT NULL,
+    edit_by INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW() NOT NULL,
+    "status" VARCHAR(20) DEFAULT 'todo' CHECK ("status" IN ('todo', 'done')) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES "user"(id),
     FOREIGN KEY (food_id) REFERENCES Food(id),
     FOREIGN KEY (edit_by) REFERENCES "user"(id)
