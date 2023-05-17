512a15a1dd0b13a46ae33852c239be58015cfaf5
wins <win4709hi@gmail.com>
2023-05-17 14:26:02 +0800

    feat: update setting

--- config/config.go
+++ config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"os"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
 	DBDriver      string `mapstructure:"DB_DRIVER"`
@@ -9,8 +13,18 @@ type Config struct {
 }
 
 func New(path string) (config Config, err error) {
+	var configName string
+	switch os.Getenv("APP_ENV") {
+	case "test":
+		configName = "test"
+	case "dev":
+		configName = "dev"
+	case "prod":
+		configName = "prod"
+	}
+
 	viper.AddConfigPath(path)
-	viper.SetConfigName("config")
+	viper.SetConfigName(configName)
 	viper.SetConfigType("env")
 
 	viper.AutomaticEnv()
