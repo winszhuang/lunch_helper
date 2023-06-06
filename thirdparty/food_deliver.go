19be4c7a020badb31b30bacdbdb05cfc85990133
wins <win4709hi@gmail.com>
2023-06-06 10:14:57 +0800

    feat: food deliver api unit test

--- thirdparty/food_deliver.go
+++ thirdparty/food_deliver.go
@@ -24,6 +24,9 @@ func NewCommonFoodDeliverApi() *CommonFoodDeliverApi {
 	return &CommonFoodDeliverApi{}
 }
 
+// url example :
+// - https://www.ubereats.com/tw/store/%E5%BA%B7%E5%AF%B6%E8%97%A5%E7%87%89%E6%8E%92%E9%AA%A8-%E7%BE%8E%E5%BE%B7%E7%B8%BD%E5%BA%97/-NQAJwmsRJGIo-7WUwHfHQ?utm_campaign=place-action-link&utm_medium=organic&utm_source=google
+// - https://www.foodpanda.com.tw/restaurant/zb5n/mu-he-tang-tai-zhong-bei-ping-dian-ri-shi-jing-gai-fan-wu-long-mian-la-mian-zhuan-mai-dian?utm_source=google&utm_medium=organic&utm_campaign=google_reserve_place_order_action
 func (c *CommonFoodDeliverApi) GetDishes(url string) ([]model.Dish, error) {
 	d, err := checkDeliver(url)
 	if err != nil {
@@ -32,9 +35,9 @@ func (c *CommonFoodDeliverApi) GetDishes(url string) ([]model.Dish, error) {
 
 	switch d {
 	case UberEats:
-		return thirdparty.GetDishesByUberEats(string(d))
+		return thirdparty.GetDishesByUberEats(url)
 	case FoodPanda:
-		return thirdparty.GetDishesByFoodPanda(string(d))
+		return thirdparty.GetDishesByFoodPanda(url)
 	default:
 		return nil, errors.New("not already implement this get dishes")
 	}
