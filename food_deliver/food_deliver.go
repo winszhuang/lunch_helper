e9fd317ebd4e1689e42fb2f0c8b40659413f738b
wins <win4709hi@gmail.com>
2023-06-17 11:26:35 +0800

    feat: add fetch limiter to avoid a lot of requests

--- food_deliver/food_deliver.go
+++ food_deliver/food_deliver.go
@@ -3,6 +3,7 @@ package food_deliver
 import (
 	"fmt"
 	"lunch_helper/food_deliver/model"
+	"time"
 )
 
 type DishesCrawler interface {
@@ -15,6 +16,9 @@ var crawlerList = []DishesCrawler{
 	&UberEatsDishesCrawler{},
 }
 
+// 限速器
+var limiter = time.Tick(200 * time.Millisecond)
+
 type FoodDeliverApi struct{}
 
 func NewFoodDeliverApi() *FoodDeliverApi {
@@ -25,6 +29,8 @@ func (f *FoodDeliverApi) GetDishesFromGoogleMap(googleMapUrl string) ([]model.Di
 	for _, crawler := range crawlerList {
 		deliverUrl, err := crawler.ParseSource(googleMapUrl)
 		if err == nil && deliverUrl != "" {
+			// 限速
+			<-limiter
 			return crawler.GetDishes(deliverUrl)
 		}
 	}
